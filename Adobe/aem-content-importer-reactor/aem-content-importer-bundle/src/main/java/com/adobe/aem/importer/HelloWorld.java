3b83ea67562711c602291b9cc819846dce1ac05a
galapagoss <gaetano@dotland.it>
2014-12-04 15:11:35 +0000

    Initial commit

---
+++ Adobe/aem-content-importer-reactor/aem-content-importer-bundle/src/main/java/com/adobe/aem/importer/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * CodeBay Innovation SL 2014
+ * aem-content-importer-bundle
+ * com.adobe.aem.importer / HelloWorld.java 
+ * Dec 4, 2014
+ * @author Gaetano
+ */
+package com.adobe.aem.importer;
+
+public class HelloWorld {
+
+}
